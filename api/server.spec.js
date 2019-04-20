8eb0f293381c87396f37be3cf82ea6036ab5b054
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-19 21:53:36 -0400

    add beforeEach refresh after each test

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,9 +1,11 @@
-// const server = require('./server.js');
+const server = require('./server.js');
 const db = require('../data/dbConfig');
-// const request = require('supertest');
+const request = require('supertest');
 
 describe('the server', () => {
-    
+    beforeEach(() => {
+        return db('movies').truncate()
+    })
     describe('the environment', () => {
         it('should set setting for the environment', () => {
             const env = process.env.DB_ENV
